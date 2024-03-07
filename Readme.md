a6ffd1d69f2301a3533d12501ebb3da5b1a73e12
artem-sereda <artem.sereda@campus.tu-berlin.de>
2024-03-08 00:40:01 +0100

    :construction

---
+++ Readme.md
@@ -0,0 +1,5 @@
+#### NeoVim Config
+
+- Based on NvChad
+- removed trash I disliek 
+- bla bla
---
+++ lua/plugins/configs/treesitter.lua
@@ -0,0 +1,35 @@
+local options = {
+  ensure_installed = {
+    "lua",
+    "vim",
+    "vimdoc",
+    "html",
+    "css",
+    "javascript",
+    "typescript",
+    "tsx",
+    "c",
+    "markdown",
+    "markdown_inline",
+    "python",
+    "go",
+    "bash",
+    "cmake",
+    "cuda",
+    "julia",
+    "rust",
+    "toml",
+    "yaml",
+    "dockerfile",
+    "terraform",
+  },
+
+  highlight = {
+    enable = true,
+    use_languagetree = true,
+  },
+
+  indent = { enable = true },
+}
+
+return options
