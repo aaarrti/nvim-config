41a79ab42c13dce82a47fca0dd225b74e44da922
artem-sereda <a.sereda@sevensenders.com>
2024-03-08 02:53:06 +0100

    :construction

--- lua/core/default_config.lua
+++ lua/core/default_config.lua
@@ -16,7 +16,7 @@ M.ui = {
   theme_toggle = { "catppuccin", "one_light" },
   changed_themes = {},
   transparency = false,
-  lsp_semantic_tokens = false, -- needs nvim v0.9, just adds highlight groups for lsp semantic tokens
+  lsp_semantic_tokens = true, -- needs nvim v0.9, just adds highlight groups for lsp semantic tokens
 
   -- https://github.com/NvChad/base46/tree/v2.0/lua/base46/extended_integrations
   extended_integrations = {}, -- these aren't compiled by default, ex: "alpha", "notify"
